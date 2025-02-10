43d731af4cef7695c364f0acfac8ab23393218c2
Mark Muthii <mark.muthii@strathmore.edu>
2025-02-10 18:06:27 +0300

    Promises example

--- test.js
+++ test.js
@@ -1,7 +1,34 @@
-let str = "This is a string. {this content will be replaced}";
+// console.log("First");
+// console.log("Second");
+// console.log("Third");
+// console.log("Fourth");
 
-console.log(str);
+// setTimeout(() => {
+//   console.log("Fifth");
+// }, 2000);
 
-str = str.replace("{this content will be replaced}", "This is the replacement");
+// console.log("Sixth");
 
-console.log(str);
+const promiseA = async (condition) => {
+  return new Promise((resolve, reject) => {
+    if (condition) {
+      resolve("Brad has kept his promise");
+    } else {
+      reject("Brad did not keep his promise");
+    }
+  });
+};
+
+try {
+  const res = await promiseA(false);
+
+  console.log("We are here in the try");
+
+  console.log(res);
+} catch (error) {
+  console.log("We are in the catch");
+
+  console.log({ error });
+}
+
+console.log("This will run");
